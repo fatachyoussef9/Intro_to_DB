cd2311566d0f59d3aefeea787e1485c73bbfa9ac
yousseffatach <youssef.allinlab@gmail.com>
2024-10-20 11:27:58 +0100

    dbb

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- Query to print the full description of the books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'books';
